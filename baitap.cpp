ce03d4c03572c297470594e8afd7bd9703c061f5
QuocTrungPhamVan <101467357+QuocTrungPhamVan@users.noreply.github.com>
2022-03-12 22:54:49 +0700

    Add files via upload

---
+++ baitap.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Ho va ten: Pham Van Quoc Trung " << '\n'
+         << "MSSV: 106210141" << '\n'
+         << "Lop: 21DTCLC2";
+    return 0;
+}
